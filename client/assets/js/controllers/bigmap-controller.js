f68a25e9164f6edf30d734c25f136d83246e8694
jiop <jeremie.iop@gmail.com>
2015-06-07 23:49:47 +0200

    Remove useless commented code

--- client/assets/js/controllers/bigmap-controller.js
+++ client/assets/js/controllers/bigmap-controller.js
@@ -9,56 +9,6 @@ angular
         $scope.myGoogleMap = {};
 
         $scope.markers = articlesService.getArticlesData();
-        // $scope.markers: [
-        //   {
-        //     id: 0,
-        //     coords: {
-        //       latitude: 48.9229643,
-        //       longitude: 2.2550196,
-        //       zoom: 17
-        //     },
-        //     title: "Colombes",
-        //     text:
-        //       "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod \
-        //       tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, \
-        //       quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo \
-        //       consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse \
-        //       cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non \
-        //       proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-        //   },
-        //   {
-        //     id: 1,
-        //     coords: {
-        //       latitude: 48.9778247,
-        //       longitude: 2.1914688,
-        //       zoom: 15
-        //     },
-        //     title: "Cormeilles",
-        //     text:
-        //       "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod \
-        //       tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, \
-        //       quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo \
-        //       consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse \
-        //       cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non \
-        //       proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-        //   },
-        //   {
-        //     id: 2,
-        //     coords: {
-        //       latitude: -37.8602828,
-        //       longitude: 145.079616,
-        //       zoom: 10
-        //     },
-        //     title: "Melbourne",
-        //     text:
-        //       "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod \
-        //       tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, \
-        //       quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo \
-        //       consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse \
-        //       cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non \
-        //       proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-        //   }
-        // ];
 
         $scope.map = {
           markers: [],
