a1101021590ed0ea04323f421d818314a3fed8c3
jiop <jeremie.iop@gmail.com>
2015-06-14 23:21:06 +0200

    Draft gallery feature

--- client/assets/js/controllers/articles-controller.js
+++ client/assets/js/controllers/articles-controller.js
@@ -2,8 +2,8 @@ angular
   .module('application.articlesController', ['truncate'])
   .controller(
     "articlesController",
-    ['$scope', '$timeout', '$http', '$rootScope', 'articlesService', '$filter',
-    function ($scope, $timeout, $http, $rootScope, articlesService, $filter) {
+    ['$scope', '$timeout', '$http', '$rootScope', 'articlesService', '$filter', '$element',
+    function ($scope, $timeout, $http, $rootScope, articlesService, $filter, $element) {
       $scope.greeting = 'Hello World!';
 
       $scope.moveMap = function(coords) {
@@ -25,6 +25,29 @@ angular
           article.textDisplayed = article.text;
           article.toggleLongText = false;
         }
+      };
+
+      $scope.gallery = {
+        index: 0,
+        currentImage: {
+          src: _.first($scope.articles).images[0].src
+        }
+      };
+
+      $scope.galleryPrev = function(article) {
+        $timeout(function() {
+          var i = ($scope.gallery.index - 1) % article.images.length;
+          if(i < 0) { i = 0; }
+          $scope.gallery.index = i;
+          $scope.gallery.currentImage.src = article.images[$scope.gallery.index].src;
+        });
+      };
+
+      $scope.galleryNext = function(article) {
+        $timeout(function() {
+          $scope.gallery.index = ($scope.gallery.index + 1) % article.images.length;
+          $scope.gallery.currentImage.src = article.images[$scope.gallery.index].src;
+        });
       };
     }]
   );
