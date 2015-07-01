442c42e2040080f9b4b7639cca6f634c82e47518
jiop <jeremie.iop@gmail.com>
2015-07-02 00:04:36 +0200

    clean gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,8 @@ var gulp     = require('gulp');
 var rimraf   = require('rimraf');
 var router   = require('front-router');
 var sequence = require('run-sequence');
+var path     = require('path');
+var env      = require('node-env-file');
 
 // Check for --production flag
 var isProduction = !!(argv.production);
@@ -75,27 +77,26 @@ gulp.task('clean', function(cb) {
 
 // Copies everything in the client folder except templates, Sass, and JS
 gulp.task('copy', function() {
-  return gulp.src(paths.assets, {
-    base: './client/'
-  })
-    .pipe(gulp.dest('./build'))
-  ;
+  return gulp
+    .src(paths.assets, { base: './client/' })
+    .pipe(gulp.dest('./build'));
 });
 
 // Copies your app's page templates and generates URLs for them
 gulp.task('copy:templates', function() {
-  return gulp.src('./client/templates/**/*.html')
+  return gulp
+    .src('./client/templates/**/*.html')
     .pipe(router({
       path: 'build/assets/js/routes.js',
       root: 'client'
     }))
-    .pipe(gulp.dest('./build/templates'))
-  ;
+    .pipe(gulp.dest('./build/templates'));
 });
 
 // Compiles the Foundation for Apps directive partials into a single JavaScript file
 gulp.task('copy:foundation', function(cb) {
-  gulp.src('bower_components/foundation-apps/js/angular/components/**/*.html')
+  gulp
+    .src('bower_components/foundation-apps/js/angular/components/**/*.html')
     .pipe($.ngHtml2js({
       prefix: 'components/',
       moduleName: 'foundation',
@@ -103,21 +104,19 @@ gulp.task('copy:foundation', function(cb) {
     }))
     .pipe($.uglify())
     .pipe($.concat('templates.js'))
-    .pipe(gulp.dest('./build/assets/js'))
-  ;
+    .pipe(gulp.dest('./build/assets/js'));
 
   // Iconic SVG icons
-  gulp.src('./bower_components/foundation-apps/iconic/**/*')
-    .pipe(gulp.dest('./build/assets/img/iconic/'))
-  ;
-
+  gulp
+    .src('./bower_components/foundation-apps/iconic/**/*')
+    .pipe(gulp.dest('./build/assets/img/iconic/'));
   cb();
 });
 
 gulp.task('copy:angular-leaflet-directive', function(cb) {
-  return gulp.src(paths.angularLeafletDirective)
-    .pipe(gulp.dest('./build/assets/js'))
-  ;
+  return gulp
+    .src(paths.angularLeafletDirective)
+    .pipe(gulp.dest('./build/assets/js'));
 });
 
 gulp.task('copy:leaflet', function(cb) {
@@ -127,13 +126,15 @@ gulp.task('copy:leaflet', function(cb) {
 });
 
 gulp.task('copy:underscore', function(cb) {
-  return gulp.src(paths.underscoreJS)
+  return gulp
+    .src(paths.underscoreJS)
     .pipe(gulp.dest('./build/assets/js'))
   ;
 });
 
 gulp.task('copy:angular-leaflet-directive', function(cb) {
-  return gulp.src(paths.angularLeafletDirective)
+  return gulp
+    .src(paths.angularLeafletDirective)
     .pipe(gulp.dest('./build/assets/js'))
   ;
 });
@@ -145,42 +146,30 @@ gulp.task('copy:fontawesome', function(cb) { 
 });
 
 gulp.task('copy:angular-truncate-2', function() {
-  return gulp.src(paths.angularTruncate2)
-    .pipe(gulp.dest('./build/assets/js/'))
-  ;
+  return gulp
+    .src(paths.angularTruncate2)
+    .pipe(gulp.dest('./build/assets/js/'));
 });
 
 // Compiles Sass
 gulp.task('sass', function () {
-  return gulp.src('client/assets/scss/app.scss')
+  return gulp
+    .src('client/assets/scss/app.scss')
     .pipe($.sass({
       includePaths: paths.sass,
-      outputStyle: 'compressed',
+      outputStyle: (isProduction ? 'compressed' : 'nested')
     }))
-//    .pipe($.sass({
-//      includePaths: paths.sass,
-//      outputStyle: (isProduction ? 'compressed' : 'nested'),
-//      errLogToConsole: true
-//    }).on('error', function (err) {
-//      console.error('Error', err.message);
-//    }))
-    .pipe($.autoprefixer({
-      browsers: ['last 2 versions', 'ie 10']
-    }))
-    .pipe(gulp.dest('./build/assets/css/'))
-  ;
+    .pipe($.autoprefixer({ browsers: ['last 2 versions', 'ie 10'] }))
+    .pipe(gulp.dest('./build/assets/css/'));
 });
 
 // Compiles and copies the Foundation for Apps JavaScript, as well as your app's custom JS
 gulp.task('uglify', ['uglify:foundation', 'uglify:app'])
 
 gulp.task('uglify:foundation', function(cb) {
-  var uglify = $.if(isProduction, $.uglify()
-    .on('error', function (e) {
-      console.log(e);
-    }));
-
-  return gulp.src(paths.foundationJS)
+  var uglify = $.if(isProduction, $.uglify().on('error', function (e) { console.log(e); }));
+  return gulp
+    .src(paths.foundationJS)
     .pipe(uglify)
     .pipe($.concat('foundation.js'))
     .pipe(gulp.dest('./build/assets/js/'))
@@ -188,12 +177,9 @@ gulp.task('uglify:foundation', function(cb) {
 });
 
 gulp.task('uglify:app', function() {
-  var uglify = $.if(isProduction, $.uglify()
-    .on('error', function (e) {
-      console.log(e);
-    }));
-
-  return gulp.src(paths.appJS)
+  var uglify = $.if(isProduction, $.uglify().on('error', function (e) { console.log(e); }));
+  return gulp
+    .src(paths.appJS)
     .pipe(uglify)
     .pipe($.concat('app.js'))
     .pipe(gulp.dest('./build/assets/js/'))
@@ -202,15 +188,16 @@ gulp.task('uglify:app', function() {
 
 // Starts a test server, which you can view at http://localhost:8080
 gulp.task('server', ['build'], function() {
-  gulp.src('./build')
+  env(path.join(__dirname, '.env'));
+  gulp
+    .src('./build')
     .pipe($.webserver({
       port: process.env.PORT,
       host: '0.0.0.0',
       fallback: 'index.html',
       livereload: false,
       open: true
-    }))
-  ;
+    }));
 });
 
 // Builds your entire app once, without starting a server
